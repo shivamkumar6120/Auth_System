f265b6aac66850895aeb2387f1cc66bda7edd989
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 13:43:32 +0530

    Users entity created

---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/entities/User.java
@@ -0,0 +1,103 @@
+package com.substring.auth.auth_app_backend.entities;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//Lombok
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "user_id")
+	private UUID id;
+
+	@Column(name = "user_email", unique = true, length = 300)
+	private String email;
+
+	@Column(name = "user_name", length = 500)
+	private String name;
+
+	private String password;
+
+	private String image;
+
+	private boolean enable = true;
+
+	private Instant createdAt = Instant.now();
+
+	private Instant updatedAt = Instant.now();
+
+	@Enumerated(EnumType.STRING)
+	private Provider provider = Provider.LOCAL;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_roles", 
+			   joinColumns = @JoinColumn(name = "user_id"), 
+			   inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<Role>();
+	
+	@PrePersist
+	protected void onCreate() {
+		Instant now = Instant.now();
+		if(now == null) {
+			createdAt = now;
+		}
+		updatedAt = now;
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		updatedAt = Instant.now();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
