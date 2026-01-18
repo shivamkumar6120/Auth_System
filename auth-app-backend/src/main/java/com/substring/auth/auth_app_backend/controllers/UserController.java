a0666d9eb3873de5326cffd385cd4464edd73916
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 19:21:26 +0530

    Controller is modified

--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.substring.auth.auth_app_backend.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,13 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<?> getAllUser(){
 		return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+	}
+	
+	@GetMapping("/email/{email}")
+	public ResponseEntity<?> getAllUserByEmail(@PathVariable("email") String email){
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.body(userService.getUserByEamil(email));
 	}
 	
 }
