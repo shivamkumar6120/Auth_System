e9d957876737c6299676936a1a9ee2e0804b393a
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 15:35:15 +0530

    User service implementation defined

---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/UserServicesImpl.java
@@ -0,0 +1,43 @@
+package com.substring.auth.auth_app_backend.services;
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+
+public class UserServicesImpl implements UserServices{
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getUserByEamil(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto updateUser(UserDto userDto, String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(String userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UserDto getUserById(String uderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
