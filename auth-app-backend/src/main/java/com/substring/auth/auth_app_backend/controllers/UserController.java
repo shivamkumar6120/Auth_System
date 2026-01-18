ad4d5b7cc8c79f3c388e6698b603bbf2d8f953d1
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 18:09:05 +0530

    Get all user api implemented

--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.substring.auth.auth_app_backend.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +25,9 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDto));
 	}
 
+	@GetMapping
+	public ResponseEntity<?> getAllUser(){
+		return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+	}
+	
 }
