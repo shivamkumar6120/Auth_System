248080307444b0131197216bdd87ef6e74652d8b
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 17:47:18 +0530

    user controller added into controller pckg

---
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.substring.auth.auth_app_backend.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+import com.substring.auth.auth_app_backend.services.UserServices;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@AllArgsConstructor
+public class UserController {
+
+	private final UserServices userService;
+
+	@PostMapping
+	public ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDto));
+	}
+
+}
