a56e795a4cfd83fe722dfd5b6a38a8aea2eda541
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-18 14:05:39 +0530

    Services added into UserServices

--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/UserServices.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/UserServices.java
@@ -1,5 +1,25 @@
 package com.substring.auth.auth_app_backend.services;
 
+import com.substring.auth.auth_app_backend.dtos.UserDto;
+
 public interface UserServices {
+	
+//	create user
+	UserDto createUser (UserDto userDto);
+	
+//	get user by email 
+	UserDto getUserByEamil(String email);
+	
+//	update user
+	UserDto updateUser(UserDto userDto, String userId);
+	
+//	delete user
+	void deleteUser(String userId);
+	
+//	get user by id
+	UserDto getUserById(String uderId);
+	
+//	get all user
+	Iterable<UserDto> getAllUsers();
 
 }
